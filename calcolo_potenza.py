96a3a8b03f199829f5a8effb6f48cf50229a0170
lorenzodex94 <161342392+lorenzodex94@users.noreply.github.com>
2024-12-20 15:09:33 +0100

    Update calcolo_potenza.py

--- calcolo_potenza.py
+++ calcolo_potenza.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 import seaborn as sns
+import streamlit as st
 
 # Title of the Streamlit app
 st.title(" Modello matematico Forno Riduttore L1")
